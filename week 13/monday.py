fc100aeea6b5915a7c62167e0906dabcd199a1df
Elizabeth Wickes <elliewix@gmail.com>
2022-11-16 10:10:23 -0600

    stuff

--- week 13/monday.py
+++ week 13/monday.py
@@ -61,9 +61,76 @@ results = [(letters.append(letter), probs.append(count / total)) for letter, cou
 # print(probs)
 # print(results) # full of nuns
 
-for _ in range(5): # make 5 names
-    result = random.choices(letters, weights=probs, k = 5)
-    print("".join(result).title())
+# for _ in range(5): # make 5 names
+#     result = random.choices(letters, weights=probs, k = 5)
+#     print("".join(result).title())
 
 # we need to generate a random length
 
+name_lengths = [len(n) for n in names]
+
+# print(list(random.sample(name_lengths, k=10)))
+
+# now can we combine this?
+
+names_with_lengths = []
+for i in range(10):
+    name = random.choice(all_letters)
+    len_choice = random.choice(name_lengths)
+    while len(name) < len_choice:
+        name += random.choice(all_letters)
+    names_with_lengths.append(name.title())
+    # print(name)
+
+print(names_with_lengths)
+
+# still doesn't look right, let's get some chunks!
+
+def ngram_letters(name, num):
+    name = name.lower()
+    nameparts = [n.strip(string.punctuation) for n in name.split()]
+
+    grams = []
+    if len(nameparts) > 1:
+        for part in nameparts:
+            # grams += ngram_letters(part,num)
+            grams.extend(ngram_letters(part,num))
+    else:
+        for i in range(len(name)):
+            if i + num > len(name):
+                break
+            grams.append(name[i:i + num])
+    return grams
+chunks = []
+
+[chunks.extend(ngram_letters(n, 2)) for n in names]
+[chunks.extend(ngram_letters(n, 3)) for n in names]
+[chunks.extend(ngram_letters(n, 4)) for n in names]
+
+chunkies = []
+
+for _ in range(10):
+    name = random.choice(chunks)
+    len_choice = random.choice(name_lengths)
+    while len(name) < len_choice:
+        name += random.choice(chunks)
+    chunkies.append(name.title())
+
+print(chunkies)
+
+# now let's try and connect things
+
+
+chunkies = []
+
+for _ in range(10):
+    name = random.choice(chunks)
+    len_choice = random.choice(name_lengths)
+    while len(name) < len_choice:
+        part_choice = random.choice(chunks)
+        while name[-1] != part_choice[0]:
+            part_choice = random.choice(chunks)
+        name += part_choice[1:]
+    chunkies.append(name.title())
+
+print(chunkies)
